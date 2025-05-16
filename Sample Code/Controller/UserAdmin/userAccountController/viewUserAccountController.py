bb9e5a0175efa5664a568c9d08a211e129f154a2
Dominic Fu Qiang Chow <162770480+dchwo@users.noreply.github.com>
2025-05-17 00:17:26 +0800

    Update viewUserAccountController.py

--- Sample Code/Controller/UserAdmin/userAccountController/viewUserAccountController.py
+++ Sample Code/Controller/UserAdmin/userAccountController/viewUserAccountController.py
@@ -1,8 +1,5 @@
 from csit314.entity.userAccount import UserAccount
 
 class ViewUserAccountController:
-    def __init__(self):
-        self.entity = UserAccount()
-
     def ViewUserAccount(self, id):
-        return self.entity.getUserDetails(self, id)
+        return self.entity.getUserDetails(id)
