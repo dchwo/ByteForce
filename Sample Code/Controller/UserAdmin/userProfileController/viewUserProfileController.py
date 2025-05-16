a97bb0b7a8f2726483c2e1430ea608478c0d7119
Dominic Fu Qiang Chow <162770480+dchwo@users.noreply.github.com>
2025-05-17 00:26:56 +0800

    Update viewUserProfileController.py

--- Sample Code/Controller/UserAdmin/userProfileController/viewUserProfileController.py
+++ Sample Code/Controller/UserAdmin/userProfileController/viewUserProfileController.py
@@ -1,8 +1,5 @@
 from csit314.entity.userProfile import UserProfile
 
 class ViewUserProfileController:
-    def __init__(self):
-        self.entity = UserProfile()
-
     def ViewUserProfile(self, id):
-        return self.entity.getDetails(self, id)
+        return self.entity.getDetails(id)
