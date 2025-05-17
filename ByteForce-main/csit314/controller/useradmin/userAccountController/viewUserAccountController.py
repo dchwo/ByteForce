f4db30a7789d95260cb40f2f105cd57bfdf52629
Dominic Fu Qiang Chow <162770480+dchwo@users.noreply.github.com>
2025-05-17 22:08:03 +0800

    Update viewUserAccountController.py

--- ByteForce-main/csit314/controller/useradmin/userAccountController/viewUserAccountController.py
+++ ByteForce-main/csit314/controller/useradmin/userAccountController/viewUserAccountController.py
@@ -1,5 +1,8 @@
 from csit314.entity.userAccount import UserAccount
 
 class ViewUserAccountController:
+    def __init__(self):
+        self.entity = UserAccount()  
+        
     def ViewUserAccount(self, id):
         return self.entity.getUserDetails(id)
