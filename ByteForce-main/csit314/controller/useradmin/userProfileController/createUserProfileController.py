1090b82100a7a83f414f6610a7950ef4426c9e7e
Dominic Fu Qiang Chow <162770480+dchwo@users.noreply.github.com>
2025-05-17 22:13:20 +0800

    Update createUserProfileController.py

--- ByteForce-main/csit314/controller/useradmin/userProfileController/createUserProfileController.py
+++ ByteForce-main/csit314/controller/useradmin/userProfileController/createUserProfileController.py
@@ -1,8 +1,8 @@
 from csit314.entity.userProfile import UserProfile
 
-class CreateUserAccountController:
+class CreateUserProfileController: 
     def __init__(self):
         self.entity = UserProfile()
 
-    def createUserProfile(self, bio, avatar, status):
-        return self.entity.add(bio, avatar, status)
+    def createUserProfile(self, role, description):
+        return self.entity.add(role, description)
