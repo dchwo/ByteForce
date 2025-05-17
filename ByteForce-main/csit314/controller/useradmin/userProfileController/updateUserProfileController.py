aee14e918e7da7b5e7a62fc5580c0bbac8afe7f7
Dominic Fu Qiang Chow <162770480+dchwo@users.noreply.github.com>
2025-05-17 19:09:07 +0800

    Update updateUserProfileController.py

--- ByteForce-main/csit314/controller/useradmin/userProfileController/updateUserProfileController.py
+++ ByteForce-main/csit314/controller/useradmin/userProfileController/updateUserProfileController.py
@@ -4,5 +4,5 @@ class UpdateUserProfileController:
     def __init__(self):
         self.entity = UserProfile()
 
-    def UpdateUserProfile(self, id):
-        return self.entity.update(id)
+    def update_profile(self, id):
+        return self.entity.update_profile(id)
