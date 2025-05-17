9d44b6449694c378d410397e9de84940b6126ab7
Dominic Fu Qiang Chow <162770480+dchwo@users.noreply.github.com>
2025-05-17 18:12:45 +0800

    Update viewUserProfileController.py

--- ByteForce-main/csit314/controller/useradmin/userProfileController/viewUserProfileController.py
+++ ByteForce-main/csit314/controller/useradmin/userProfileController/viewUserProfileController.py
@@ -2,4 +2,4 @@ from csit314.entity.userProfile import UserProfile
 
 class ViewUserProfileController:
     def ViewUserProfile(self, id):
-        return self.entity.get_profile_by_id(id)
+        return self.entity.get_profile_by_user_id(id)
