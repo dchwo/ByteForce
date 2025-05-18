bafb75d36424258cc0a98185a282f671ed61fd39
Cher2803 <cherwei.ong@gmail.com>
2025-05-18 21:30:00 +0800

    Update searchListingController.py

--- ByteForce-main/csit314/controller/homeowner/searchListingController.py
+++ ByteForce-main/csit314/controller/homeowner/searchListingController.py
@@ -5,6 +5,5 @@ class SearchListingController:
         self.cleanerListing = CleanerListing()
        
     # searchListingController
-    def searchListing(self, name, email, service_name, min_price, max_price, type):
-        return self.cleanerListing.search_service_listings(name, email, service_name, min_price, max_price, type)
-    
+    def searchListing(self, name, email, service_name, min_price, max_price, type, category_id):
+        return self.cleanerListing.search_service_listings(name, email, service_name, min_price, max_price, type, category_id)
