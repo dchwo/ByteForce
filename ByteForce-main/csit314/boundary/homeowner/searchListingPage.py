741ca3d4b7911ee5b0c3e17736a7a8944bb2e50d
Cher2803 <cherwei.ong@gmail.com>
2025-05-18 21:31:02 +0800

    Update searchListingPage.py

--- ByteForce-main/csit314/boundary/homeowner/searchListingPage.py
+++ ByteForce-main/csit314/boundary/homeowner/searchListingPage.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, session, flash
 from csit314.controller.homeowner.searchShortlistController import SearchShortlistController
 from csit314.controller.homeowner.searchListingController   import SearchListingController
+from csit314.controller.platformmanager.searchServiceCategoryController import SearchServiceCategoryController
 
 searchListingPage = Blueprint('searchListingPage', __name__, template_folder='../templates', static_folder='../static')
 searchShortlistController = SearchShortlistController()
 searchListingController   = SearchListingController()
+searchServiceCategoryController = SearchServiceCategoryController()
 
 # searchListingPage
 @searchListingPage.route('/browse_services')
@@ -22,6 +24,9 @@ def searchListing():
     min_price = request.args.get('min_price')
     max_price = request.args.get('max_price')
     type = request.args.get('type')
+    category_id = request.args.get('category_id')
+
+    categories = searchServiceCategoryController.searchServiceCategories(None, "active")
 
     shortlisted_services = searchShortlistController.searchShortlist(user_id)
     shortlisted_ids = [service['id'] for service in shortlisted_services] if shortlisted_services else []
@@ -30,9 +35,9 @@ def searchListing():
         if not shortlisted_ids:
             services = []
         else:
-            all_services = searchListingController.searchListing(name, email, service_name, min_price, max_price, type)
+            all_services = searchListingController.searchListing(name, email, service_name, min_price, max_price, type, category_id)
             services = [s for s in all_services if s['id'] in shortlisted_ids]
     else:
-        services = searchListingController.searchListing(name, email, service_name, min_price, max_price, type)
-        
-    return render_template('browse_services.html', services=services, shortlisted_ids=shortlisted_ids, first_name=first_name)
+        services = searchListingController.searchListing(name, email, service_name, min_price, max_price, type, category_id)
+
+    return render_template('browse_services.html', services=services, shortlisted_ids=shortlisted_ids, categories=categories, first_name=first_name)
