1737d5a5b60024cf232da4298ef0de4eb9fe1fe0
Dominic Fu Qiang Chow <162770480+dchwo@users.noreply.github.com>
2025-05-18 15:23:10 +0800

    Update searchUserProfileController.py

--- ByteForce-main/csit314/controller/useradmin/userProfileController/searchUserProfileController.py
+++ ByteForce-main/csit314/controller/useradmin/userProfileController/searchUserProfileController.py
@@ -3,6 +3,6 @@ from csit314.entity.userProfile import UserProfile
 class SearchUserProfileController:
     def __init__(self):
         self.entity = UserProfile()
-
-    def SearchUserProfile(self, id, role):
-        return self.entity.search_profiles(id, role)
+        
+   def SearchUserProfile(self, id=None, role=None):
+    return self.entity.search_profiles(id=id, role=role, first_name=None, last_name=None, email=None, status=None)
