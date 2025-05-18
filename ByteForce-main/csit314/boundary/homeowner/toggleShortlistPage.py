5c67ca970e739a54c7907f1785587b080cfdb598
Cher2803 <cherwei.ong@gmail.com>
2025-05-18 21:11:23 +0800

    Update toggleShortlistPage.py

--- ByteForce-main/csit314/boundary/homeowner/toggleShortlistPage.py
+++ ByteForce-main/csit314/boundary/homeowner/toggleShortlistPage.py
@@ -10,8 +10,8 @@ searchShortlistController = SearchShortlistController()
 
 # toggleShortlistPage
 # Save/Undo shortlist button
-@toggleShortlistPage.route('/shortlist/<int:listing_id>', methods=['POST'])
-def toggleShortlist(listing_id):
+@toggleShortlistPage.route('/shortlist/<int:listing_id>/<int:fromShortlistPage>', methods=['POST'])
+def toggleShortlist(listing_id, fromShortlistPage):
     if 'user_id' not in session:
         return redirect('/user_login')
     
@@ -25,9 +25,11 @@ def toggleShortlist(listing_id):
         createShortlistController.createShortlist(user_id, listing_id)
         flash("Listing saved to shortlist.", "success")
 
+
     # Preserve shortlist filter if set
-    if request.args.get("shortlisted") or (request.referrer and "shortlisted=1" in request.referrer):
+    if fromShortlistPage:
+        return redirect('/my_shortlisted_services')
+    elif request.args.get("shortlisted") or (request.referrer and "shortlisted=1" in request.referrer):
         return redirect('/browse_services?shortlisted=1')
     else: 
         return redirect('/browse_services')    
-
